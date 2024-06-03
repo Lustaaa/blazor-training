1de4622da72fff0bd843b46ba80a1ea81dbbecb8
Lustaaa <free.lusta@gamil.com>
2024-06-03 14:37:20 +0200

    Exercice1

---
+++ Exercice1/Exercice1/Pages/ComposantEnfant.razor.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Exercice1.Pages
+{
+    public partial class ComposantEnfant
+    {
+        [Parameter]
+        public string Name { get; set; } = "";
+        [Parameter]
+        public EventCallback<List<string>> RetourEnfant { get; set; }
+
+        public bool Visible { get; set; } = false;
+
+        public int IndexQuestion { get; set; } = 0;
+        public string Question { get; set; } = "";
+        public List<string> Questions { get; set; } = new List<string>() { "Question 1", "Question 2" };
+
+        public List<string> Reponses { get; set; } = new List<string>();
+
+        protected override void OnInitialized()
+        {
+            Visible = false;
+        }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            if (!string.IsNullOrWhiteSpace(Name))
+            {
+                Visible = true;
+                Reponses.Add($"Les réponses de {Name} sont :");
+                Question = Questions[IndexQuestion];
+            }
+        }
+
+        private void Add(bool response)
+        {
+            Reponses.Add(response ? "Oui" : "Non");
+
+            if (IndexQuestion >= Questions.Count - 1)
+            {
+                RetourEnfant.InvokeAsync(Reponses);
+                Reponses.Clear();
+                IndexQuestion = 0;
+                Visible = false;
+                Name = string.Empty;
+            }
+            else
+            {
+                IndexQuestion++;
+                Question = Questions[IndexQuestion];
+            }
+        }
+    }
+}
---
+++ Exercice1/Exercice1/Pages/ComposantParent.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Xml.Linq;
+
+namespace Exercice1.Pages
+{
+    public partial class ComposantParent
+    {
+        public string Name { get; set; }
+
+        public List<string> ReponseEnfants { get; set; } = new List<string>();
+
+        public void RetourEnfant(List<string> reponse)
+        {
+            Name = string.Empty;
+            ReponseEnfants.Clear();
+            reponse.ForEach(r => { ReponseEnfants.Add(r); });
+        }
+    }
+}
